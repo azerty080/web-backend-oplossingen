a0ee73a01d23744cf02750c83128d323d513f58a
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-09-26 12:09:10 +0200

    added oplossing-comments

---
+++ oplossing-comments.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Oplossing comments</title>
+    </head>
+
+    <body>
+
+        <p><?php echo Niels van Nimmen ?></p>
+    
+    </body>
+</html>
