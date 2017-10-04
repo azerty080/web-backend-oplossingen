75d9bfdd3bb0ecb2cf8c43b8a33c9044aea1a3c8
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-04 19:13:25 +0200

    solved oplossing-string-concatenate

---
+++ oplossing-string-concatenate.php
@@ -0,0 +1,34 @@
+<?php
+	
+	$voornaam = "Niels";
+	$achternaam = "van Nimmen";
+
+	$volledigenaam = $voornaam . $achternaam;
+
+	$aantalkarakters = strlen( $volledigenaam );
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Voorbeeld concatenatie</title>
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+
+<body  class="web-backend-inleiding">
+	
+	<section class="body">
+
+		<p><?php echo $volledigenaam ?></p>
+		
+		<p><?php echo $aantalkarakters ?></p>
+		
+	</section>
+
+</body>
+</html>
