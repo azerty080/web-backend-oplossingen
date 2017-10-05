f45baee6da8bf5668c8eca2be3b0febf06d063f2
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-05 12:14:48 +0200

    added oplossing-array-functions-deel1

---
+++ oplossing-array-functions-deel1.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht array functies</title> 
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+
+
+            
+            <h1>Deel 1</h1>
+
+            <ul>
+                <li>Maak een array waarin je meer dan 5 dieren plaatst</li>
+
+                <li>Laat het script berekenen hoeveel elementen er in de array zitten en druk af naar het scherm</li>
+
+                <li>Maak het mogelijk om met een variabele <code>$teZoekenDier</code> een dier te zoeken in de array, druk tevens een gepaste boodschap af (gevonden/niet gevonden).</li>
+
+            </ul> 
+
+
+        </section>
+
+    </body>
+</html>
