7fc6850ed16e31316e36c322466fe51659f72f02
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-05 12:10:12 +0200

    solved oplossing-array-basis-deel2

--- oplossing-array-basis-deel2.php
+++ oplossing-array-basis-deel2.php
@@ -1,6 +1,45 @@
 <?php
 
-    
+    $getallen = array(1, 2, 3, 4, 5);
+
+    $vermenigvuldiging = $getallen[0] * $getallen[1] * $getallen[2] * $getallen[3] * $getallen[4];
+
+    $oneven = '';
+    $space = ' ';
+
+    if ($getallen[0] % 2 != 0)
+    {
+        $oneven .= $getallen[0] . $space;
+    }
+
+    if ($getallen[1] % 2 != 0)
+    {
+        $oneven .= $getallen[1] . $space;
+    }
+
+    if ($getallen[2] % 2 != 0)
+    {
+        $oneven .= $getallen[2] . $space;
+    }
+
+    if ($getallen[3] % 2 != 0)
+    {
+        $oneven .= $getallen[3] . $space;
+    }
+
+    if ($getallen[4] % 2 != 0)
+    {
+        $oneven .= $getallen[4] . $space;
+    }
+
+
+    $getallenRev = array(5, 4, 3, 2, 1);
+
+    $indexSom[] = $getallen[0] + $getallenRev[0];
+    $indexSom[] = $getallen[1] + $getallenRev[1];
+    $indexSom[] = $getallen[2] + $getallenRev[2];
+    $indexSom[] = $getallen[3] + $getallenRev[3];
+    $indexSom[] = $getallen[4] + $getallenRev[4];
 
 ?>
 
@@ -19,7 +58,9 @@
         <section class="body">
         
             <h1>Deel 2</h1>
-
+            <p><?php echo $vermenigvuldiging ?></p>
+            <p><?php echo $oneven ?></p>
+            <pre><?php echo var_dump($indexSom) ?></pre>
         
         </section>
 
