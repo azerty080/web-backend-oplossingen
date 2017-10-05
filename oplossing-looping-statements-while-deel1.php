ccd17c38c02e66e01c8cf6c8efbe0809423a04c2
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-05 12:43:40 +0200

    added oplossing-looping-statements-while-deel1

---
+++ oplossing-looping-statements-while-deel1.php
@@ -0,0 +1,35 @@
+<?php
+
+    $space = ' ,';
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht while</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">  
+        
+        <h1>Deel 1</h1>
+
+
+
+
+
+            <li>Druk alle getallen af van 0 tot 100 afgescheiden door een komma en 
+            een spatie ' , '.</li>
+
+            <li>Op een volgende lijn druk je alle getallen af die deelbaar zijn door 3 én groter zijn dan 40 mààr kleiner zijn dan 80.</li>
+
+
+
+    </body>
+</html>
