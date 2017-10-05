065f3cf71c617cf16410fa3de10949d31576c4a8
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-05 10:37:52 +0200

    added oplossing-conditional-statements-if-else-deel1

---
+++ oplossing-conditional-statements-if-else-deel1.php
@@ -0,0 +1,27 @@
+<?php
+    
+    
+    
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht if else</title> 
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>Deel1</h1>
+            <p><?php  ?></p>
+
+        </section>
+
+    </body>
+</html>
