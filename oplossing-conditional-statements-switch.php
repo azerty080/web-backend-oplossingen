c8bc3551b05eb0d3affecdf37f0e888f46e525fe
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-05 11:32:00 +0200

    solved oplossing-conditionals-statements-switch

--- oplossing-conditional-statements-switch.php
+++ oplossing-conditional-statements-switch.php
@@ -1,6 +1,41 @@
 <?php
 
+    $getal = 1;
 
+    switch ($getal)
+    {
+        case 1:
+            $dag = 'maandag';
+            break;
+
+        case 2:
+            $dag = 'dinsdag';
+            break;
+
+        case 3:
+            $dag = 'woensdag';
+            break;
+
+        case 4:
+            $dag = 'donderdag';
+            break;
+
+        case 5:
+            $dag = 'vrijdag';
+            break;
+
+        case 6:
+            $dag = 'zaterdag';
+            break;
+
+        case 7:
+            $dag = 'zondag';
+            break;
+        
+        default:
+            $dag = 'ongeldig';
+            break;
+    }
 
 ?>
 
@@ -18,7 +53,7 @@
         
         <section class="body">
         
-            <p><?php  ?></p>
+            <p><?php echo $dag ?></p>
 
         </section>
 
