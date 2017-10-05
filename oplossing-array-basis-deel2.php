d1a8ff9eead6b15982791b864f2e2e306dac2b25
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-05 11:47:19 +0200

    added oplossing-array-basis-deel2

---
+++ oplossing-array-basis-deel2.php
@@ -0,0 +1,27 @@
+<?php
+
+    
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht arrays basis</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+        
+            <h1>Deel 2</h1>
+
+        
+        </section>
+
+    </body>
+</html>
