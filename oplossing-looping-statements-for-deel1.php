66d66b536b484e1b4ab25b9627287592c191a9ac
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-08 14:27:30 +0200

    added oplossing-looping-statements-for-deel1

---
+++ oplossing-looping-statements-for-deel1.php
@@ -0,0 +1,28 @@
+<?php
+
+    
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht for</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>Deel 1</h1>
+
+            
+
+        </section>
+
+    </body>
+</html>
