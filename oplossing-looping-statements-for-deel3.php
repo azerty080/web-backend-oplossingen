4467b91915b58984ad6e81644e3704ef08deda41
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-08 15:01:24 +0200

    added oplossing-looping-statements-for-deel3

---
+++ oplossing-looping-statements-for-deel3.php
@@ -0,0 +1,50 @@
+<?php
+
+    $rijen = 10;
+    $kolommen = 10;
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht for</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+        <style>
+            .oneven
+            {
+                background-color    :   lightgreen;
+            }
+        </style>
+
+            <h1>Deel 3</h1>
+
+            <table>
+                <?php
+                    for ($i=0; $i <= $rijen ; $i++)
+                    { 
+                        $tafel = $i;
+                        echo '<tr>';
+                        for ($j=0; $j < $kolommen ; $j++)
+                        { 
+                            echo '<td class=' . (($tafel % 2 == 0 ) ? '' : 'oneven') . '>' . $tafel . '</td>';
+                            $tafel += $i;
+                        }
+                        echo '</tr>';
+                    }
+                ?>
+            </table>
+
+        </section>
+
+    </body>
+</html>
