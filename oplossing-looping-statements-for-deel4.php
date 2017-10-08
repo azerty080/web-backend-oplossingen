5e4df3e15e940ebecab5a0b0a9a9333c5f318ca3
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-08 19:28:58 +0200

    added oplossing-looping-statement-for-deel4

---
+++ oplossing-looping-statements-for-deel4.php
@@ -0,0 +1,60 @@
+<?php
+
+    $rijen = 10;
+    $kolommen = 10;
+
+    for ($i=0; $i <= $rijen; $i++)
+    { 
+        $kolommenArray = array();
+
+        for ($j=0; $j <= $kolommen; $j++)
+        { 
+            $kolommenArray[] = $i * $j;
+        }
+
+        $rijenArray[ $i ] = $kolommenArray;
+    }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht for</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+        <style>
+            .oneven
+            {
+                background-color    :   lightgreen;
+            }
+        </style>
+
+            <h1>Deel 4</h1>
+
+            <table>
+                <?php
+                    foreach ($rijenArray as $kolommenArray)
+                    {
+                        echo '<tr>';
+                        foreach ($kolommenArray as $kolom)
+                        {
+                            echo '<td class=' . (($kolom % 2 == 0 ) ? '' : 'oneven') . '>' . $kolom . '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                ?>
+            </table>
+
+        </section>
+
+    </body>
+</html>
