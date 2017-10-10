7fd35de50f5795c568b5f2ba3518ebcc54b64dee
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-10 10:30:25 +0200

    solved oplossing-functions-gevorderd-deel1

--- oplossing-functions-gevorderd-deel1.php
+++ oplossing-functions-gevorderd-deel1.php
@@ -1,6 +1,49 @@
 <?php
 
-    
+    $md5HashKey = 'd1fa402db91a7a93c4f414b8278ce073';
+
+    $parameter1 = '2';
+    $parameter2 = '8';
+    $parameter3 = 'a';
+
+    function findParameter1($hash, $parameter)
+    {
+        $length = strlen($hash);
+
+        $parameterAmount = substr_count($hash, $parameter);
+
+        $parameterProcent = ($parameterAmount / $length) * 100;
+
+        return $parameterProcent;
+    }
+
+
+    function findParameter2($parameter)
+    {
+        global $md5HashKey;
+
+        $length = strlen($md5HashKey);
+
+        $parameterAmount = substr_count($md5HashKey, $parameter);
+
+        $parameterProcent = ($parameterAmount / $length) * 100;
+
+        return $parameterProcent;
+    }
+
+
+    function findParameter3($parameter)
+    {
+        $hash = $GLOBALS['md5HashKey'];
+
+        $length = strlen($hash);
+
+        $parameterAmount = substr_count($hash, $parameter);
+
+        $parameterProcent = ($parameterAmount / $length) * 100;
+
+        return $parameterProcent;
+    }
 
 ?>
 
@@ -20,7 +63,9 @@
         
             <h1>Deel 1</h1>
 
-
+            <p>Functie 1: De needle <?php echo $parameter1 ?> komt <?php echo findParameter1($md5HashKey, $parameter1) ?>% voor in de hash key <?php echo $md5HashKey ?></p>
+            <p>Functie 2: De needle <?php echo $parameter2 ?> komt <?php echo findParameter2($parameter2) ?>% voor in de hash key <?php echo $md5HashKey ?></p>
+            <p>Functie 3: De needle <?php echo $parameter3 ?> komt <?php echo findParameter3( $parameter3) ?>% voor in de hash key <?php echo $md5HashKey ?></p>
 
         </section>
 
