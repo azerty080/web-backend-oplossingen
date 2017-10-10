c852cf11778318b18a5f4884eb8bbd4a39c07875
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-10 12:23:01 +0200

    added oplossing-functions-recursive-deel1

---
+++ oplossing-functions-recursive-deel1.php
@@ -0,0 +1,36 @@
+<?php
+
+    
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht recursive</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <style>
+                img
+                {
+                    display:block;
+                    padding:6px;
+                    margin:24px auto;
+                }
+            </style>
+        
+            <h1>Deel 1</h1>
+
+
+        </section>
+
+    </body>
+</html>
