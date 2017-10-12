27cf85ab00227242381d81e6bdb624f945367a07
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-12 10:49:47 +0200

    edited oplossing-get and added folder images with images

--- oplossing-get.php
+++ oplossing-get.php
@@ -1,6 +1,12 @@
 <?php
 
-    
+    $artikels = array(
+
+'Ikea gaat zonnepanelen verkopen in ons land (en je hoeft ze niet zelf te installeren)' => 'Meubelreus Ikea gaat vanaf vandaag zonnepanelen verkopen in ons land. Eerst online en vanaf eind januari ook in de acht Belgische warenhuizen van de Zweedse keten.', '', '', 
+
+'“Radarverklikkers mogen niet te precies zijn”' => 'Het federaal verkeersveiligheidsinstituut Vias wil dat apps zoals Coyote en Waze stoppen met de exacte locatie van snelheidscontroles te melden. “Laat hen enkel nog waarschuwen voor een flitspaal in de ruime omgeving”, klinkt het.', '', '', 
+
+'Nul begrip voor sluiting Olmense Zoo' => 'De sluiting van de Olmense Zoo heeft gisteren een ongeziene storm aan reacties teweeggebracht. Heel wat mensen steunen de minister in zijn beslissing, maar in Balen en (verre) omgeving kan de beslissing op geen begrip rekenen. Zelfs burgemeester Johan Leysen (CD&V) maakte openlijk bekend dat hij de beslissing van Ben Weyts héél betreurenswaardig en onbegrijpelijk vindt.', '', '');
 
 ?>
 
@@ -18,7 +24,7 @@
         
         <section class="body">
         
-            
+
 
 
 
