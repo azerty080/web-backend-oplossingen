0f3225e1345cf41e594603d10a4e207581e271f5
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-16 14:43:03 +0200

    solved oplossing-post

--- oplossing-post.php
+++ oplossing-post.php
@@ -1,6 +1,24 @@
 <?php
 
-    
+    $password = 'azerty';
+    $username = 'niels';
+
+    $message = '';
+
+    if (isset($_POST['password']) == True && isset($_POST['username']) == True)
+    {
+        $enteredPassword = $_POST['password'];
+        $enteredUsername = $_POST['username'];
+
+        if ($enteredPassword == $password && $enteredUsername == $username)
+        {
+            $message = 'Welkom';
+        }
+        else
+        {
+            $message = 'Er ging iets mis bij het inloggen, probeer opnieuw';
+        }
+    }
 
 ?>
 
@@ -17,8 +35,24 @@
     <body class="web-backend-opdracht">
         
         <section class="body">
-        
             
+            <h1>Inloggen</h1>
+
+            <form action="oplossing-post.php" method="post">
+                <ul>
+                    <li>
+                        <label for="username">Gebruikersnaam</label>
+                        <input type="text" name="username" id="username">
+                    </li>
+                    <li>
+                        <label for="password">Passwoord</label>
+                        <input type="text" name="password" id="password">
+                    </li>
+                </ul>
+                <input type="submit" name="submit">
+            </form>
+
+            <?php echo $message ?>
 
         </section>
 
