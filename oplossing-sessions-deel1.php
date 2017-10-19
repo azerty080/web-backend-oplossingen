6a9591d0ba0f26798b587828c5e571a216c23819
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-19 11:39:28 +0200

    added oplossing-sessions-deel1

---
+++ oplossing-sessions-deel1.php
@@ -0,0 +1,23 @@
+<?php
+
+    
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht sessions</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <h1>Deel 1</h1>
+
+
+        
+    </body>
+</html>
