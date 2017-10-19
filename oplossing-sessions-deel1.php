ce378470e6624f2b490b440752a059c67cff1e5b
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-19 12:09:54 +0200

    solved oplossing-sessions-deel1

--- oplossing-sessions-deel1.php
+++ oplossing-sessions-deel1.php
@@ -1,6 +1,17 @@
 <?php
 
-    
+    session_start();
+
+    if (isset($_POST['email']))
+    {
+        $_SESSION['email'] = $_POST['email'];
+    }
+
+
+    if (isset($_POST['nickname']))
+    {
+        $_SESSION['nickname'] = $_POST['nickname'];
+    }
 
 ?>
 
@@ -15,9 +26,25 @@
     </head>
     <body class="web-backend-opdracht">
         
-        <h1>Deel 1</h1>
+        <section class="body">
 
+            <h1>Deel 1: registratiegegevens</h1>
 
+            <form method="POST">
+                <ul>
+                    <li>
+                        <label for="email">e-mail</label>
+                        <input type="text" name="email">
+                    </li>
+                    <li>
+                        <label for="nickname">nickname</label>
+                        <input type="text" name="nickname">
+                    </li>
+                </ul>
+                <input type="submit" value="Volgende">
+            </form>
+
+        </section>
         
     </body>
 </html>
