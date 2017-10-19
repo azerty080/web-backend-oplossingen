d884bd74ccf1286b6a9bbd12a2514d4b931ade1f
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-19 10:55:04 +0200

    added oplossing-date-deel1

---
+++ oplossing-date-deel1.php
@@ -0,0 +1,23 @@
+<?php
+
+    
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht date</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+    </head>
+    <body class="web-backend-opdracht">
+        <section class="body">
+            <h1>Deel 1</h1>
+
+
+
+        </section>
+    </body>
+</html>
