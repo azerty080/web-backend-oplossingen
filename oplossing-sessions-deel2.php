2ac85917f08ecf3c8e34add32cbc8a83899a0809
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-19 12:11:56 +0200

    added oplossing-sessions-deel2

---
+++ oplossing-sessions-deel2.php
@@ -0,0 +1,57 @@
+<?php
+
+    session_start();
+
+    if (isset($_POST['email']))
+    {
+        $_SESSION['email'] = $_POST['email'];
+    }
+
+
+    if (isset($_POST['nickname']))
+    {
+        $_SESSION['nickname'] = $_POST['nickname'];
+    }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht sessions</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>Registratiegegevens</h1>
+
+            <ul>
+                <li>e-mail: <?php  ?></li>
+                <li>nickname: <?php  ?></li>
+            </ul>
+
+            <h1>Deel 2: adres</h1>
+
+            <form method="POST">
+                <ul>
+                    <li>
+                        <label for="email">e-mail</label>
+                        <input type="text" name="email">
+                    </li>
+                    <li>
+                        <label for="nickname">nickname</label>
+                        <input type="text" name="nickname">
+                    </li>
+                </ul>
+                <input type="submit" value="Volgende">
+            </form>
+
+        </section>
+        
+    </body>
+</html>
