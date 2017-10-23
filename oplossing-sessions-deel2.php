525b0d25e20678837409813e49b60ad804a61773
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-10-23 14:46:34 +0200

    solved oplossing-sessions-deel2

--- oplossing-sessions-deel2.php
+++ oplossing-sessions-deel2.php
@@ -2,16 +2,56 @@
 
     session_start();
 
-    if (isset($_POST['email']))
+    $email = '';
+    $nickname = '';
+
+    if (isset($_SESSION['email']))
     {
-        $_SESSION['email'] = $_POST['email'];
+        $email = $_SESSION['email'];
+    }
+
+    if (isset($_SESSION['nickname']))
+    {
+        $nickname = $_SESSION['nickname'];
+    }
+    
+
+
+    if (isset($_GET['session']))
+    {
+        if($_GET['session'] === 'destroy')
+        {
+            session_destroy();
+            header('Location: oplossing-sessions-deel2.php');
+        }
     }
 
 
-    if (isset($_POST['nickname']))
+    
+    if (isset($_POST['straat']))
     {
-        $_SESSION['nickname'] = $_POST['nickname'];
+        $_SESSION['straat'] = $_POST['straat'];
     }
+
+    if (isset($_POST['nummer']))
+    {
+        $_SESSION['nummer'] = $_POST['nummer'];
+    }
+
+    if (isset($_POST['gemeente']))
+    {
+        $_SESSION['gemeente'] = $_POST['gemeente'];
+    }
+
+    if (isset($_POST['postcode']))
+    {
+        $_SESSION['postcode'] = $_POST['postcode'];
+    }
+
+    $straat = isset($_SESSION['straat']) ? $_SESSION['straat'] : '';
+    $nummer = isset($_SESSION['nummer']) ? $_SESSION['nummer'] : '';
+    $gemeente = isset($_SESSION['gemeente']) ? $_SESSION['gemeente'] : '';
+    $postcode = isset($_SESSION['postcode']) ? $_SESSION['postcode'] : '';
 
 ?>
 
@@ -31,8 +71,8 @@
             <h1>Registratiegegevens</h1>
 
             <ul>
-                <li>e-mail: <?php  ?></li>
-                <li>nickname: <?php  ?></li>
+                <li>e-mail: <?php echo $email ?></li>
+                <li>nickname: <?php echo $nickname ?></li>
             </ul>
 
             <h1>Deel 2: adres</h1>
@@ -40,16 +80,27 @@
             <form method="POST">
                 <ul>
                     <li>
-                        <label for="email">e-mail</label>
-                        <input type="text" name="email">
+                        <label for="straat">straat</label>
+                        <input type="text" id="straat" name="straat" value="<?php echo $straat ?>">
                     </li>
                     <li>
-                        <label for="nickname">nickname</label>
-                        <input type="text" name="nickname">
+                        <label for="nummer">nummer</label>
+                        <input type="number" id="nummer" name="nummer" value="<?php echo $nummer ?>">
+                    </li>
+                    <li>
+                        <label for="gemeente">gemeente</label>
+                        <input type="text" id="gemeente" name="gemeente" value="<?php echo $gemeente ?>">
+                    </li>
+                    <li>
+                        <label for="postcode">postcode</label>
+                        <input type="text" id="postcode" name="postcode" value="<?php echo $postcode ?>">
                     </li>
                 </ul>
                 <input type="submit" value="Volgende">
             </form>
+
+            <h1>Destroy session</h1>
+            <a href="http://oplossingen.web-backend.local/oplossing-sessions-deel2.php?session=destroy">Destroy session</a>
 
         </section>
         
