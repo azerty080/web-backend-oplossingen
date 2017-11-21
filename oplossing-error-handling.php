ea4bfb2d33947c24157ae0c861d06e383e72711f
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-11-21 21:19:30 +0100

    solved oplossing-error-handling

--- oplossing-error-handling.php
+++ oplossing-error-handling.php
@@ -1,6 +1,98 @@
 <?php
 
-    
+    session_start();
+
+    $isValid = FALSE;
+
+    try
+    {
+        if (isset($_POST['submit']))
+        {
+            if (isset($_POST['code']))
+            {
+                if (strlen($_POST['code']) >= 8)
+                {
+                    $isValid = TRUE;
+                }
+                else
+                {
+                    throw new Exception("VALIDATION-CODE-LENGTH");
+                }
+            }
+            else
+            {
+                throw new Exception("SUBMIT-ERROR");
+            }
+        }
+    }
+    catch (Exception $e)
+    {
+        $messageCode = $e->showMessage();
+
+        $message = array();
+
+        $createMessage = FALSE;
+
+        switch ($messageCode)
+        {
+            case 'SUBMIT-ERROR':
+                $message['type'] = 'error';
+                $message['text'] = 'Er werd met het formulier geknoeid';
+                break;
+            
+            case 'VALIDATION-CODE-LENGTH':
+                $message['type'] = 'error';
+                $message['text'] = 'De kortingscode heeft niet de juiste lengte';
+
+                $createMessage = TRUE;
+                break;
+        }
+
+        if ($createMessage)
+        {
+            createMessage($message);
+        }
+
+        logToFile($message);
+    }
+
+
+
+    function logToFile($message)
+    {
+        $date = '[' . date('H:i:s', time()).']';
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $type = $message['type'];
+        $text = $message['text'];
+
+        $errorMessage = $date . ' - ' . $ip . ' - type:[' . $type . '] - ' . $text . "\n\r";
+
+        file_put_contents('log.txt', $errorMessage);
+    }
+
+    function createMessage($message)
+    {
+        $_SESSION['message'] = $message;
+    }
+
+    function showMessage()
+    {
+        if (isset($_SESSION['message']))
+        {
+            $message = $_SESSION['message'];
+            unset($_SESSION['message']);
+
+            return $message;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+
+
+    $message = showMessage();
 
 ?>
 
@@ -20,7 +112,33 @@
         
 			<h1>Deel 1</h1>
 
+            <?php
 
+                if ($message)
+                {
+                    echo "<p class=\"modal " . $message['type'] . " \">" . $message['text'] . "</p>";
+                }
+
+            ?>
+
+            <?php if(!$isValid): ?>
+
+                <h1>Geef uw kortingscode op</h1>
+                <form action="#" method="POST">
+                    <ul>
+                        <li>
+                            <label for="code">Kortingscode</label>
+                            <input type="text" id="code" name="code">
+                        </li>
+                    </ul>
+                    <input type="submit">
+                </form>
+
+            <?php else: ?>
+
+                <p>Korting toegekend!</p>
+
+            <?php endif ?>
 
 		</section>
 
