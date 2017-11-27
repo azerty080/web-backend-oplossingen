86ed4333b88f7b208375c93f39d513be97b89437
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-11-27 14:20:30 +0100

    solved oplossing-crud-insert

--- oplossing-crud-insert.php
+++ oplossing-crud-insert.php
@@ -1,5 +1,38 @@
 <?php
 
+	$message = '';
+
+	if (isset($_POST['submit']))
+	{
+		try
+		{
+			$db = new PDO('mysql:host=localhost;dbname=bieren', 'root', '', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+			$queryString = 'INSERT INTO brouwers (brnaam, adres, postcode, gemeente, omzet) VALUES (:brnaam, :adres, :postcode, :gemeente, :omzet)';
+
+			$statement = $db->prepare($queryString);
+
+			$statement->bindValue(':brnaam', $_POST['brnaam']);
+			$statement->bindValue(':adres', $_POST['adres']);
+			$statement->bindValue(':postcode', $_POST['postcode']);
+			$statement->bindValue(':gemeente', $_POST['gemeente']);
+			$statement->bindValue(':omzet', $_POST['omzet']);
+
+			
+			if ($statement->execute())
+			{
+				$message = 'Brouwerij succesvol toegevoegd. Het unieke nummer van deze brouwerij is ' . $db->lastInsertId();
+			}
+			else
+			{
+				$message = 'Er ging iets mis met het toevoegen. Probeer opnieuw.';
+			}
+		}
+		catch (PDOException $e)
+		{
+			$message = 'Er ging iets mis ' . $e->getMessage();
+		}
+	}
 	
 
 ?>
@@ -18,7 +51,35 @@
         
         <section class="body">
 
+        	<h1>Voeg een brouwer toe</h1>
 
+        	<p><?php echo $message ?></p>
+
+			<form action="oplossing-crud-insert.php" method="POST">
+				<ul>
+					<li>
+						<label for="brouwernaam">Brouwernaam</label>
+						<input type="text" id="brouwernaam" name="brnaam">
+					</li>
+					<li>
+						<label for="adres">adres</label>
+						<input type="text" id="adres" name="adres">
+					</li>
+					<li>
+						<label for="postcode">postcode</label>
+						<input type="text" id="postcode" name="postcode">
+					</li>
+					<li>
+						<label for="gemeente">gemeente</label>
+						<input type="text" id="gemeente" name="gemeente">
+					</li>
+					<li>
+						<label for="omzet">omzet</label>
+						<input type="text" id="omzet" name="omzet">
+					</li>
+				</ul>
+				<input type="submit" name="submit">
+			</form>
 
         </section>
 
