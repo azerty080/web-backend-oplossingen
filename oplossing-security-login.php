d897c1175d8ef4d80d971f2e34f9835e2b8c603d
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-12-01 12:02:14 +0100

    added oplossing-security-login

---
+++ oplossing-security-login.php
@@ -0,0 +1,26 @@
+<?php
+
+	
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht security: login</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+        	
+
+        </section>
+
+    </body>
+</html>
