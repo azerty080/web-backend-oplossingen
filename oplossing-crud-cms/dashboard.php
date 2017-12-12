c169997e92bf24dcfa98e9cd8152c9548b853065
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-12-12 19:34:50 +0100

    added folder oplossing-crud-cms with copied files from oplossing-secutiry-login

---
+++ oplossing-crud-cms/dashboard.php
@@ -0,0 +1,97 @@
+<?php
+
+    $email = '';
+    $hashed_salted_email = '';
+    $message = '';
+
+    $isValid = FALSE;
+
+
+	if(!isset($_COOKIE['login']))
+    {
+        header('Location: login-form.php');
+    }
+    else
+    {
+        $cookieData = explode(',', $_COOKIE['login']);
+        $email = $cookieData[0];
+        $hashed_email = $cookieData[1];
+    }
+
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=opdracht_crud_cms', 'root', '', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+        $queryString = 'SELECT * FROM users WHERE email = :email';
+
+        $statement = $db->prepare($queryString);
+
+        $statement->bindValue(':email', $email);
+
+        $statement->execute();
+        
+
+        $userData = array();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+            $userData[] = $row;
+        }
+
+
+        if (!empty($userData))
+        {
+            $salt = $userData[0]['salt'];
+            $hashed_email_reference = hash('sha512', $email . $salt);
+
+            if ($hashed_email_reference == $hashed_email)
+            {
+                $isValid = TRUE;
+            }
+            else
+            {
+                setcookie("login", $email . ',' . $hashed_email, time()-3600);
+            }
+        }
+        else
+        {
+            $message = 'Dit email adres is nog niet in gebruik';
+        }
+
+    }
+    catch (PDOException $e)
+    {
+        $message = 'Er ging iets mis ' . $e->getMessage();
+    }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dashboard</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <?php if ($isValid): ?>
+
+                <h1>Dashboard</h1>
+                <a href="http://oplossingen.web-backend.local/oplossing-security-login/logout.php">uitloggen</a>
+
+            <?php endif ?>
+
+
+            <?php if ($message != '') { echo '<p>' . $message . '</p>';} ?>
+
+
+        </section>
+
+    </body>
+</html>
---
+++ oplossing-crud-cms/registratie-form.php
@@ -0,0 +1,77 @@
+<?php
+
+    session_start();
+
+
+
+    $email = '';
+    $password = '';
+    $message = array();
+
+    if (isset($_SESSION['registration']))
+    {
+        $email = $_SESSION['registration']['email'];
+        $password = $_SESSION['registration']['password'];
+    }
+
+    if (isset($_SESSION['notification']['email']))
+    {
+        $message[] = $_SESSION['notification']['email'];
+    }
+
+    if (isset($_SESSION['notification']['password']))
+    {
+        $message[] = $_SESSION['notification']['password'];
+    }
+
+    if (isset($_SESSION['notification']['message']))
+    {
+        $message[] = $_SESSION['notification']['message'];
+    }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Registratie form</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>Registreren</h1>
+
+            <?php
+
+                foreach ($message as $value)
+                {
+                    echo '<p>' . $value . '</p>';
+                }
+
+            ?>
+
+            <form action="registratie-process.php" method="POST">
+                <ul>
+                    <li>
+                        <label for="email">e-mail</label>
+                        <input type="text" name="email" id="email" value="<?php echo $email ?>">
+                    </li>
+                    <li>
+                        <label for="password">paswoord</label>
+                        <input type="<?php ($password == '')? 'password' : 'text' ?>" name="password" id="password" value="<?php echo $password ?>">
+                        <input type="submit" name="generatePassword" value="Genereer een paswoord">
+                    </li>
+                </ul>
+                <input type="submit" name="submit" value="Registreer">
+            </form>
+
+        </section>
+
+    </body>
+</html>
