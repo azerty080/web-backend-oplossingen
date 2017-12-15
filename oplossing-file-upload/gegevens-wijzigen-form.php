dbeda38b2f675dc844e8d5201820b5b305f80588
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-12-15 09:41:46 +0100

    added gegevens-wijzigen-form for oplossing-file-upload

---
+++ oplossing-file-upload/gegevens-wijzigen-form.php
@@ -0,0 +1,103 @@
+<?php
+
+    $email = '';
+    $hashed_salted_email = '';
+    $message = '';
+
+    $isValid = FALSE;
+
+
+	if(!isset($_COOKIE['login']))
+    {
+        header('Location: login-form.php');
+    }
+    else
+    {
+        $cookieData = explode(',', $_COOKIE['login']);
+        $email = $cookieData[0];
+        $hashed_email = $cookieData[1];
+    }
+
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=opdracht_file_upload', 'root', '', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+        $queryString = 'SELECT * FROM users WHERE email = :email';
+
+        $statement = $db->prepare($queryString);
+
+        $statement->bindValue(':email', $email);
+
+        $statement->execute();
+        
+
+        $userData = array();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+            $userData[] = $row;
+        }
+
+
+        if (!empty($userData))
+        {
+            $salt = $userData[0]['salt'];
+            $hashed_email_reference = hash('sha512', $email . $salt);
+
+            if ($hashed_email_reference == $hashed_email)
+            {
+                $isValid = TRUE;
+            }
+            else
+            {
+                setcookie("login", $email . ',' . $hashed_email, time()-3600);
+            }
+        }
+        else
+        {
+            $message = 'Dit email adres is nog niet in gebruik';
+        }
+
+    }
+    catch (PDOException $e)
+    {
+        $message = 'Er ging iets mis ' . $e->getMessage();
+    }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Gegevens wijzigen</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <?php if ($isValid): ?>
+
+                <p><a href="http://oplossingen.web-backend.local/oplossing-file-upload/dashboard.php">Terug naar dashboard</a> | Ingelogd als <?php echo $email ?> | <a href="http://oplossingen.web-backend.local/oplossing-file-upload/logout.php">uitloggen</a></p>   
+
+                <h1>Dashboard</h1>
+                
+                <ul>
+                    <li><a href="">Artikels</a></li>
+                    <li><a href="http://oplossingen.web-backend.local/oplossing-file-upload/gegevens-wijzigen-form.php">Gegevens wijzigen</a></li>
+                </ul>
+
+            <?php endif ?>
+
+
+            <?php if ($message != '') { echo '<p>' . $message . '</p>';} ?>
+
+
+        </section>
+
+    </body>
+</html>
