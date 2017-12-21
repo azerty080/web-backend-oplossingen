cd95ab90ddb8f0b75b29a2e5a8c03d070d08b4a2
Niels van Nimmen <niels.van.nimmen@gmail.com>
2017-12-21 22:30:02 +0100

    added oplossing-ajax

---
+++ oplossing-ajax/contact-form.php
@@ -0,0 +1,70 @@
+<?php
+
+    session_start();
+
+
+    if (isset($_SESSION['enteredData']))
+    {
+        $email = $_SESSION['enteredData']['email'];
+        $formMessage = $_SESSION['enteredData']['formMessage'];
+        $copy = $_SESSION['enteredData']['copy'];
+    }
+
+    if (isset($_SESSION['message']))
+    {
+        $message = $_SESSION['message'];
+
+        unset($_SESSION['message']);
+    }
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contact form</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>Contacteer ons</h1>
+
+            <?php
+
+                if (isset($message))
+                {
+                    echo '<p>' . $message . '</p>';
+                }
+
+            ?>
+
+            <form action="contact.php" method="POST">
+                
+                <ul>
+                    <li>
+                        <label for="email">Emailadres</label>
+                        <input type="text" id="email" name="email" value="<?php echo (isset($email)) ? $email : '' ?>">
+                    </li>
+                    <li>
+                        <label for="message">Bericht</label>
+                        <textarea id="message" name="message" cols="30" rows="10"><?php echo (isset($formMessage)) ? $formMessage : '' ?></textarea>
+                    </li>
+                    <li>
+                        <label for="send-copy">Stuur kopie naar mezelf</label>
+                        <input type="checkbox" id="send-copy" name="send-copy" <?php  echo (isset($copy)) ? 'checked' : ''?>>
+                    </li>
+                </ul>
+                
+                <input type="submit" name="submit">
+            </form>
+
+        </section>
+
+    </body>
+</html>
---
+++ oplossing-ajax/contact.php
@@ -0,0 +1,78 @@
+<?php
+
+    session_start();
+
+    if (isset($_POST['submit']))
+    {
+        $admin = 'emailadres.vanadmin@gmail.com';
+
+        $email = $_POST['email'];
+        $formMessage = $_POST['message'];
+
+        $_SESSION['enteredData']['email'] = $_POST['email'];
+        $_SESSION['enteredData']['formMessage'] = $_POST['message'];
+        $_SESSION['enteredData']['copy'] = $_POST['send-copy'];
+
+        if (isset($_POST['send-copy']))
+        {
+            $copy = TRUE;
+        }
+        else
+        {
+            $copy = FALSE;
+        }
+
+        try
+        {
+            $db = new PDO('mysql:host=localhost;dbname=opdracht_mail', 'root', '', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+            $queryString = 'INSERT INTO contact_messages (email, message, time_sent) VALUES ("' . $email . '", "' . $formMessage . '", NOW())';
+
+            $statement = $db->prepare($queryString);
+
+            $statement->bindValue(':email', $email);
+
+
+            if ($statement->execute())
+            {
+
+                $subject = 'Bericht van ' . $email;
+
+                $messageSent = mail($admin, $subject, $message);
+
+                $copySent = TRUE;
+
+                if ($copy)
+                {
+                    $subject = 'Copy van bericht van ' . $email;
+
+                    $copySent = mail($email, $subject, $message);
+                }
+
+                if ($messageSent || $copySent)
+                {
+                    $message = 'Je bericht is verzonden';
+
+                    unset($_SESSION['enteredData']);
+                }
+                else
+                {
+                    $message = 'Er ging iets mis bij het verzenden van je bericht';
+                }
+            }
+            else
+            {
+                $message = 'Er ging iets mis';
+            }
+        }
+        catch (Exception $e)
+        {
+            $message = 'Er ging iets mis ' . $e->getMessage();
+        }
+
+        $_SESSION['message'] = $message;
+    }
+
+    header('location: contact-form.php');
+
+?>
