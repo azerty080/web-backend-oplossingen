5de6883e13acdd64a6e40727caa101211b9bf8de
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-02 16:59:19 +0100

    added oplossing-regular-expressions

---
+++ oplossing-regular-expressions.php
@@ -0,0 +1,42 @@
+<?php
+
+
+    
+
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht Regular Expressions</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>RegEx tester</h1>
+
+            <form action="">
+
+            <ul>
+                <li>
+                    <label for="regex">Regular Expression</label>
+                    <input type="text" name="regex" id="regex">
+                </li>
+                <li>
+                    <label for="string">String</label>                            
+                    <textarea name="string" id="string" cols="30" rows="10"></textarea>
+                </li>
+            </ul>
+
+            </form>
+
+        </section>
+    </body>
+</html>
