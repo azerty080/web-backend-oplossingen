77d5e4c56a6bec6cfd6dbe12f5f3f1543daedf17
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-03 15:09:15 +0100

    solved oplossing-regular-expressions

--- oplossing-regular-expressions.php
+++ oplossing-regular-expressions.php
@@ -1,5 +1,18 @@
 <?php
 
+    $regex = '';
+    $string = '';
+    $result = '';
+
+    if (isset($_POST['submit']))
+    {
+        $regex = $_POST['regex'];
+        $string = $_POST['string'];
+
+        $replacement = '<span>#</span>';
+
+        $result = preg_replace('/' . $regex . '/', $replacement, $string);
+    }
 
     
 
@@ -22,20 +35,51 @@
 
             <h1>RegEx tester</h1>
 
-            <form action="">
-
-            <ul>
-                <li>
-                    <label for="regex">Regular Expression</label>
-                    <input type="text" name="regex" id="regex">
-                </li>
-                <li>
-                    <label for="string">String</label>                            
-                    <textarea name="string" id="string" cols="30" rows="10"></textarea>
-                </li>
-            </ul>
-
+            <form action="oplossing-regular-expressions.php" method="POST">
+                <ul>
+                    <li>
+                        <label for="regex">Regular Expression</label>
+                        <input type="text" name="regex" id="regex" value="">
+                    </li>
+                    <li>
+                        <label for="string">String</label>                            
+                        <textarea type="text" name="string" id="string" value=""></textarea>
+                    </li>
+                </ul>
+                <input type="submit" name="submit">
             </form>
+
+            <?php
+
+                if ($result != '')
+                {
+                    echo '<p>' . $result . '</p>';
+                }
+
+            ?>
+
+
+
+            <h1>Regex 1</h1>
+            <p>/[a-du-zA-DU-Z]/</p>
+            <p>Memory can change the shape of a room; it can change the color of a car. And memories can be distorted. They're just an interpretation, they're not a record, and they're irrelevant if you have the facts</p>
+
+
+            <h1>Regex 2</h1>
+            <p>colou?r</p>
+            <p>Zowel colour als color zijn correct Engels</p>
+
+
+            <h1>Regex 3</h1>
+            <p>1\d{3}</p>
+            <p>1020 1050 9784 1560 0231 1546 8745</p>
+
+
+            <h1>Regex 4</h1>
+            <p>[0-9]{2}[\/\-\.][0-9]{2}[\/\-\.][0-9]{4}</p>
+            <p>([0-9]{1,4}[\-\.\/]?)+</p>
+            <p>(\d{1,4}\S){3}</p>
+            <p>24/07/1978 en 24-07-1978 en 24.07.1978</p>
 
         </section>
     </body>
