e5c854f7f02aa08d7e67f92c8cc589e038f25fd3
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-04 19:10:10 +0100

    solved oplossing-mod-rewrite-basis deel3

---
+++ oplossing-mod-rewrite-basis/deel3/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht mod_rewrite: basis</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+
+            <h1>Index</h1>
+
+            <?php var_dump( $_GET ) ?>
+
+        </section>
+        
+    </body>
+</html>
