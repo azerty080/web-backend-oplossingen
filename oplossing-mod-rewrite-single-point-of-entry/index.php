29e52a1a8d5dd47739a98baf38a8d982739ef216
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-08 22:45:07 +0100

    solved oplossing-mod-rewrite-single-point-of-entry

--- oplossing-mod-rewrite-single-point-of-entry/index.php
+++ oplossing-mod-rewrite-single-point-of-entry/index.php
@@ -4,4 +4,65 @@
     var_dump($_GET);
 
 
+    function __autoload($className)
+    {
+        include_once 'classes/' . $className . '.php';
+    }
+
+
+
+    if (isset($_GET['controller']))
+    {
+        $controller = $_GET['controller'];
+    }
+    else
+    {
+        $controller = 'overview';
+    }
+
+
+    if (isset($_GET['method']))
+    {
+        $method = $_GET['method'];
+    }
+    else
+    {
+        $method = 'method';
+    }
+
+
+
+    if (isset($_GET['id']))
+    {
+        $id = $_GET['id'];
+    }
+    else
+    {
+        $id = 1;
+    }
+
+
+
+    $bieren = new Bieren();
+
+
+    switch($method)
+    {
+        case 'insert':
+            $insert = $bieren->insert();
+            break;
+
+        case 'delete':
+            $delete = $bieren->delete();
+            break;
+
+        case 'update':
+            $update = $bieren->update();
+            break;
+        
+        default:
+            $overview = $bieren->overview();
+    }
+
+
 ?>
