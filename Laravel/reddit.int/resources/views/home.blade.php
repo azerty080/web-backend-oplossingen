f4f9812999181bc46af07ccf5cf2f6d3bc82e94b
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-17 16:57:43 +0100

    added home, instructies, login and register page file

---
+++ Laravel/reddit.int/resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 10px;
+            }
+
+            nav {
+                margin: 0;
+            }
+
+            a {
+                text-decoration: none;
+                margin: 0 10px 0 10px;
+            }
+
+            h1 {
+                margin: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <nav>
+            <a href="http://oplossingen.web-backend.local/Laravel/reddit.int/public/">Hackernews.local</a>
+            <a href="http://oplossingen.web-backend.local/Laravel/reddit.int/public/home">Home</a>
+            <a href="http://oplossingen.web-backend.local/Laravel/reddit.int/public/instructies">Instructies</a>
+            <a href="http://oplossingen.web-backend.local/Laravel/reddit.int/public/login">Login</a>
+            <a href="http://oplossingen.web-backend.local/Laravel/reddit.int/public/register">Register</a>
+            <a href="">Name</a>
+        </nav>
+
+        <h1>Home</h1>
+    </body>
+</html>
